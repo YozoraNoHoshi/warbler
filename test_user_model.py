9d410313d3d60640ab5de2797792ceba7c731d3c
P Virk <virk@outlook.com>
2018-12-07 12:25:25 -0800

    User Model tests written

--- test_user_model.py
+++ test_user_model.py
@@ -4,7 +4,6 @@
 #
 #    python -m unittest test_user_model.py
 
-
 import os
 from unittest import TestCase
 
@@ -16,7 +15,6 @@ from models import db, User, Message, FollowersFollowee
 # connected to the database
 
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
-
 
 # Now we can import app
 
@@ -41,18 +39,121 @@ class UserModelTestCase(TestCase):
 
         self.client = app.test_client()
 
+        self.u = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD")
+
+        db.session.add(self.u)
+
+        self.u2 = User(
+            email="test2@test.com",
+            username="testuser2",
+            password="HASHED_PASSWORD")
+
+        db.session.add(self.u2)
+        db.session.commit()
+
     def test_user_model(self):
         """Does basic model work?"""
 
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
+        # User should have no messages & no followers
+        self.assertEqual(len(self.u.messages), 0)
+        self.assertEqual(len(self.u.followers), 0)
 
-        db.session.add(u)
+    def test_repr(self):
+        '''Testing for repr, using string from Models.py'''
+
+        self.assertEqual(
+            repr(self.u), f"<User #{self.u.id}: testuser, test@test.com>")
+
+    def test_is_following(self):
+        '''Added two users above, append the relationship, test using "following" '''
+
+        self.u.following.append(self.u2)
         db.session.commit()
 
-        # User should have no messages & no followers
-        self.assertEqual(len(u.messages), 0)
-        self.assertEqual(len(u.followers), 0)
+        self.assertEqual(self.u.is_following(self.u2), True)
+
+    def test_is_not_following(self):
+        '''Removed append'''
+
+        self.assertEqual(self.u.is_following(self.u2), False)
+
+    def test_is_followed_by(self):
+        '''Added two users above, append the relationship, test using  "followed" '''
+
+        self.u.followers.append(self.u2)
+        db.session.commit()
+
+        self.assertEqual(self.u.is_followed_by(self.u2), True)
+
+    def test_is_not_followed_by(self):
+        '''Removed append '''
+
+        self.assertEqual(self.u.is_followed_by(self.u2), False)
+
+    def test_create_User(self):
+        '''Creating a new user and manually checking password hash'''
+
+        from flask_bcrypt import Bcrypt
+        bcrypt = Bcrypt()
+
+        sign_up_test = User.signup(
+            username="test",
+            password="test123",
+            email="abc@abc.com",
+            image_url="/test.jpg")
+
+        db.session.commit()
+
+        self.assertEqual(sign_up_test.username, 'test')
+        self.assertEqual(
+            bcrypt.check_password_hash(sign_up_test.password, "test123"), True)
+        self.assertEqual(sign_up_test.email, 'abc@abc.com')
+        self.assertEqual(sign_up_test.image_url, '/test.jpg')
+
+    def test_create_User_fail(self):
+        '''Checking if it raises ValueError when password is not passed. "as cm" should let you capture the type of error raised but we couldnt get it work'''
+
+        with self.assertRaises(ValueError):
+            User.signup(
+                username="test",
+                password="",
+                email="abc@abc.com",
+                image_url="/test.jpg")
+
+            # the_exception = cm.exception
+            # self.assertEqual(the_exception.error_code, 3)
+
+    def test_user_authenticate(self):
+        '''Tests if user is returned when authenticated, authenticate function accepts username and password'''
+
+        testuser3 = User.signup(
+            username="testuser3",
+            email="abc@abc.com",
+            password="123456",
+            image_url="/test.jpg")
+
+        db.session.commit()
+
+        self.assertEqual(
+            User.authenticate(username="testuser3", password="123456"),
+            testuser3)
+
+    def test_fail_user_authenticate(self):
+        '''Tests if an error is returned if credentials dont match'''
+
+        testuser3 = User.signup(
+            username="testuser3",
+            email="abc@abc.com",
+            password="123456",
+            image_url="/test.jpg")
+
+        db.session.commit()
+
+        self.assertEqual(
+            User.authenticate(username="testuser3", password="123abc"), False)
+
+        self.assertEqual(
+            User.authenticate(username="testuser4", password="123456"), False)
